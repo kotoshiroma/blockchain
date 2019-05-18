efc566ca800d5dd0f98360cd4cc1c7571302a8aa
kotoshiroma <kotoshiroma@gmail.com>
2019-05-18 23:10:02 +0900

    Blockchainクラスの主要な関数およびプロパティの実装

--- blockchain.py
+++ blockchain.py
@@ -1,24 +1,48 @@
-# class Blockchain(object): # (object)は何なのか？ => pythonの公式ドキュメント（クラス定義）に記載あった
-# 基底クラス object を継承
-class Blockchain:
+from time import time
+import json
+import hashlib
+
+class Blockchain(object):
   def __init__(self):
     self.chain = []
     self.current_transactions = []
 
-  def new_block(self):
-    # 新しいブロックを作り、チェーンに加える
-    pass
+    # ジェネシスブロックを作る
+    self.new_block(previous_hash=1, proof=100)
 
-  def new_transaction(self):
+  def new_block(self, proof, previous_hash=None):
+    # 新しいブロックを作り、チェーンに加える
+    block = {
+      'index': len(self.chain) + 1,
+      'timestamp': time(),
+      'transactions': self.current_transactions,
+      'proof': proof,
+      'previous_hash': previous_hash
+    }
+
+    # 現在のトランザクションをリセット（ブロックへのセットが完了したため）
+    self.current_transactions = []
+
+    self.chain.append(block)
+    return block
+
+  def new_transaction(self, sender, recipient, amount):
     # 新しいトランザクションをリストに加える
-    pass
+    self.current_transactions.append({
+      'sender': sender,
+      'recipient': recipient,
+      'amount': amount
+    })
+
+    return self.last_block['index'] + 1 # このトランザクションを含む（はずの？）ブロックのindex
 
   @staticmethod
   def hash(block):
     # ブロックをハッシュ化する
-    pass
-
+    block_byte = json.dumps(block, sort_keys=True).encode()
+    return hashlib.sha256(block_byte).hexdigest()
+    
   @property
   def last_block(self):
     # チェーンの最後のブロックを返す
-    pass
+    return self.chain[-1]
